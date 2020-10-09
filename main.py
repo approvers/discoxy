94958a9a989f4488b4f13afa9f7283d514e2b8ae
Colk-tech <colk.tech@gmail.com>
2020-10-09 17:46:14 +0900

    [変更] 書き直しのお時間

--- main.py
+++ main.py
@@ -1,30 +1,73 @@
 import sys
-import tkinter
+
+from tkinter import *
+from tkinter import filedialog
+from tkinter.ttk import *
+
+PROGRAM_NAME: str = "discoxy"
 
 
-def main():
-    root = tkinter.Tk()
-    root.title("Discoxy")
-    root.geometry("450x200")
+class Root:
+    def __init__(self):
+        self.window = Tk()
+        self.window.title(PROGRAM_NAME)
+        self.window.geometry("450x200")
+        self.window.grid_rowconfigure(0, weight=1)
+        self.window.grid_columnconfigure(0, weight=1)
 
-    label = tkinter.Label(text="HTTPプロキシ:")
-    label.pack()
+        main_page = MainPage(self.window)
 
-    edit_box = tkinter.Entry(width=30)
-    edit_box.pack()
+        main_page.frame.tkraise()
 
-    label2 = tkinter.Label(text="ポート番号:")
-    label2.pack()
+        self.window.mainloop()
 
-    edit_box2 = tkinter.Entry(width=30)
-    edit_box2.pack()
 
-    button = tkinter.Button(text="起動", width=10)
-    button.bind("<button-1>", None)
-    button.pack()
+class MainPage:
+    def __init__(self, r: Tk):
+        """
+        Args:
+            r(tk.Tk): tk.Tk()で取得できるwindow
+        """
+        self.frame = Frame(r)
 
-    root.mainloop()
+        PROXY_DESCRIPTION = "HTTPプロキシ:"
+        PORT_DESCRIPTION = "ポート番号:"
+        PLACE_DESCRIPTION = "Discordの場所"
+
+        proxy_label = Label(self.frame, text=PROXY_DESCRIPTION)
+        port_label = Label(self.frame, text=PORT_DESCRIPTION)
+        place_label = Label(self.frame, text=PLACE_DESCRIPTION)
+
+        proxy_entry = Entry(self.frame)
+        proxy_entry.insert(END, "")
+        port_entry = Entry(self.frame)
+        port_entry.insert(END, "")
+        place_entry = Entry(self.frame)
+        place_entry.insert(END, "")
+
+        start_button = Button(self.frame, text="起動", command=self.start)
+
+        proxy_label.pack()
+        proxy_entry.pack()
+        port_label.pack()
+        port_entry.pack()
+        place_label.pack()
+        place_entry.pack()
+        Utils.space(self.frame, 1)
+        start_button.pack()
+
+        self.frame.grid(row=0, column=0, sticky="nsew")
+
+    def start(self):
+        print("test")
+
+
+class Utils:
+    @staticmethod
+    def space(frame: Frame, count: int):
+        for _ in range(count):
+            Label(frame, text="").pack()
 
 
 if __name__ == "__main__":
-    main()
+    Root()
