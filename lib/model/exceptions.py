4e8cb44b9daf8372e7f34265c77f1694f2f38f2c
Colk-tech <colk.tech@gmail.com>
2020-10-09 21:44:20 +0900

    [追加] バリデーションを追加

--- lib/model/exceptions.py
+++ lib/model/exceptions.py
@@ -3,6 +3,7 @@ class InvalidPlatformError(RuntimeError):
         super().__init__("Invalid platform! Current platform name: \"{0}\"".format(platform_name))
         self.platform_name = platform_name
 
+
 class DiscordNotFoundError(RuntimeError):
     def __init__(self, expected_path: str):
         super().__init__("Discord not found! Expected path: \"{0}\"".format(expected_path))
--- lib/view.py
+++ lib/view.py
@@ -1,5 +1,11 @@
+import os
+from typing import Optional
 from tkinter import *
 from tkinter.ttk import *
+from tkinter.messagebox import *
+from urllib import parse
+
+from lib import controller
 
 PROGRAM_NAME: str = "discoxy"
 
@@ -26,6 +32,7 @@ class MainPage:
             r(tk.Tk): tk.Tk()で取得できるwindow
         """
         self.frame = Frame(r)
+        self.root = r
 
         PROXY_DESCRIPTION = "HTTPプロキシ:"
         PORT_DESCRIPTION = "ポート番号:"
@@ -39,7 +46,7 @@ class MainPage:
         self.proxy_entry.insert(END, "")
         self.port_entry = Entry(self.frame)
         self.port_entry.insert(END, "")
-        self.place_entry = Entry(self.frame)
+        self.place_entry = Entry(self.frame, width=40)
         self.place_entry.insert(END, "")
 
         start_button = Button(self.frame, text="起動", command=self.start)
@@ -56,11 +63,33 @@ class MainPage:
         self.frame.grid(row=0, column=0, sticky="nsew")
 
     def start(self):
-        law_proxy: str = str(self.proxy_entry.get())
-        law_port: int = int(self.port_entry.get())
-        law_place: str = str(self.place_entry.get())
+        self.parser(self.proxy_entry.get(), self.place_entry.get(), self.port_entry.get())
 
-        print(law_place, law_port, law_proxy)
+    def parser(self, law_proxy: str, law_place: str, law_port: str):
+        if not Utils.is_url(law_proxy):
+            Utils.error_dialog("Invalid address has passed. Please specify valid URL.")
+            return
+
+        try:
+            port = int(law_port)
+        except Exception as e:
+            Utils.error_dialog(e)
+            return
+
+        if not (0 <= port <= 65535):
+            Utils.error_dialog("Invalid port number has passed. Please specify within 0 ~ 65535.")
+            return
+
+        try:
+            if not os.path.exists(law_place):
+                raise FileNotFoundError("File not found. Please check your file.")
+        except Exception as e:
+            Utils.error_dialog(e)
+            return
+
+        DataClass.law_proxy = str(self.proxy_entry.get())
+        DataClass.law_place = str(self.place_entry.get())
+        DataClass.law_port = int(self.port_entry.get())
 
 
 class Utils:
@@ -68,3 +97,18 @@ class Utils:
     def space(frame: Frame, count: int):
         for _ in range(count):
             Label(frame, text="").pack()
+
+    @staticmethod
+    def is_url(text):
+        url_param = parse.urlparse(text)
+        return len(url_param.scheme) > 0
+
+    @staticmethod
+    def error_dialog(message: str):
+        showerror(message=message)
+
+
+class DataClass:
+    law_proxy: Optional[str] = None
+    law_port: Optional[int] = None
+    law_place: Optional[str] = None
