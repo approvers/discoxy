a2bb0651f86c4e7d08f9fb60690fda1d70e3fe72
Colk-tech <colk.tech@gmail.com>
2020-10-09 16:56:02 +0900

    [追加] 超基本的なところができた

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello, world!")
+import sys
+import tkinter
+
+
+def main():
+    root = tkinter.Tk()
+    root.title("Discoxy")
+    root.geometry("450x200")
+
+    label = tkinter.Label(text="HTTPプロキシ:")
+    label.pack()
+
+    edit_box = tkinter.Entry(width=30)
+    edit_box.pack()
+
+    label2 = tkinter.Label(text="ポート番号:")
+    label2.pack()
+
+    edit_box2 = tkinter.Entry(width=30)
+    edit_box2.pack()
+
+    button = tkinter.Button(text="起動", width=10)
+    button.bind("<button-1>", None)
+    button.pack()
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
